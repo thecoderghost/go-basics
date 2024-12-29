a4295a161996fd55576b66ac2adfdc12cb1c715f
thecoderghost <apple.lokeshchandra@gmail.com>
2024-12-29 18:42:48 +0530

    added hello world

---
+++ hello-world/go.mod
@@ -0,0 +1,3 @@
+module github.com/thecoderghost/go-basics/hello-world
+
+go 1.21.1
---
+++ hello-world/main.go
@@ -0,0 +1,11 @@
+// Define main package
+package main
+
+import "fmt"
+
+// Start writing code
+// Writing main function to execute code
+func main() {
+	// Using fmt package to print hello world
+	fmt.Println("Hello World!")
+}
